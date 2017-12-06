0d88d9de72cd58067895733e214577344416a57e
Jacob Stevens <cobymstevens44@gmail.com>
2017-12-06 13:43:15 -0500

    Started OOP PHP 2.

---
+++ PHPOOP2/index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+  <?php
+  class Alligator{
+    private $color
+    private $length
+    private $prey
+    function Alligator(){
+      display()
+    }
+  }
+  ?>
+</body>
+</html>
